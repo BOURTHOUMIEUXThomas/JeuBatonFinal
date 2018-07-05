1fb8e00288f96425ebf64ebf58888173b5cabf2f
btxth <btxth@DESKTOP-9EKH27I>
2018-07-05 18:28:34 +0200

    Commit 3

--- JeuBatonFinal/src/main/java/fr/exia/JeuBatonFinal/Player.java
+++ JeuBatonFinal/src/main/java/fr/exia/JeuBatonFinal/Player.java
@@ -1,5 +1,50 @@
 package fr.exia.JeuBatonFinal;
+import java.util.Scanner;
+
 
 public class Player {
 
+	
+		public int SoustractionNumber;
+		public static Stick stick; 
+		
+		
+		public Player (Stick stick) {
+			SoustractionNumber = 0;
+			setStick(stick);
+		}
+
+
+		public int getSoustractionNumber() {
+			return SoustractionNumber;
+		}
+
+
+		public void setSoustractionNumber(int soustractionNumber) {
+			SoustractionNumber = soustractionNumber;
+		}
+
+
+		public static Stick getStick() {
+			return stick;
+		}
+
+
+		public static void setStick(Stick stick) {
+			Player.stick = stick;
+		}
+		
+		public int PlayerTurn() {
+			Scanner sc = new Scanner(System.in);
+			setSoustractionNumber(sc.nextInt());
+			return getSoustractionNumber();
+			
+		}
+		
+		public int IA (int StickNumber) {
+			if nb_baton
+			return this.getSoustractionNumber();
+			
+		}
+		
 }
